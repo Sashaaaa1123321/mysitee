92e7575ad915483f59e2e1bdf3393f55f11b076d
Sashaaaa1123321 <da209rova@gmail.com>
2026-03-14 03:07:00 +0200

    Create script.js

---
+++ style/script.js
@@ -0,0 +1,13 @@
+function buyNow(){
+
+alert("Thanks for your interest! Online store coming soon.")
+
+}
+
+function scrollToProduct(){
+
+document.getElementById("product").scrollIntoView({
+behavior:"smooth"
+})
+
+}
